2b3246297a99598aa98040a2f71051be57d233f6
Mikita Syradoyeu <mikita_syradoyeu@epam.com>
2026-01-13 17:36:48 +0300

    oop test

---
+++ oop/bind-call-aply-test.ts
@@ -0,0 +1,22 @@
+const person = {
+  name: "Nikita",
+  sayHello: function () {
+    console.log(`Hello hello, ${this.name}`);
+  },
+};
+
+const sayHelloFunc = person.sayHello;
+
+function greet(this: { name: string }, greeting: string) {
+  console.log(greeting + ", " + this.name);
+}
+
+const person1 = {
+  name: "Vladimir",
+};
+
+sayHelloFunc.bind(person1)();
+
+greet.bind(person1, "Hi")();
+greet.apply(person1, ["Hello"]);
+greet.call(person1, "Whats up");
